5fa312f4206c26cf5c7588b7a05925c873473bbc
Holmes <crazy_runcheng@163.com>
2016-01-20 10:47:25 +0800

    Revert "dao wrap"
    This reverts commit 906ce5371ad80621730c9abf5fee7864b1c8f0c6.

--- autofile/dao/RiskPremiumDao.java
+++
@@ -1,7 +0,0 @@
-package daoPackageName;
-public class RiskPremiumDaoDao{
-    public void insertRiskPremiumDao(RiskPremiumDao riskPremiumDao);
-    public List<RiskPremiumDao> selectRiskPremiumDaoList();
-    public void updateRiskPremiumDao(RiskPremiumDao riskPremiumDao);
-    public void deleteRiskPremiumDaoById(Integer id);
-    }
