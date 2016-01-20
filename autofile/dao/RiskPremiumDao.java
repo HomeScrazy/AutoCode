906ce5371ad80621730c9abf5fee7864b1c8f0c6
Holmes <crazy_runcheng@163.com>
2016-01-20 10:44:06 +0800

    dao wrap

---
+++ autofile/dao/RiskPremiumDao.java
@@ -0,0 +1,7 @@
+package daoPackageName;
+public class RiskPremiumDaoDao{
+    public void insertRiskPremiumDao(RiskPremiumDao riskPremiumDao);
+    public List<RiskPremiumDao> selectRiskPremiumDaoList();
+    public void updateRiskPremiumDao(RiskPremiumDao riskPremiumDao);
+    public void deleteRiskPremiumDaoById(Integer id);
+    }
